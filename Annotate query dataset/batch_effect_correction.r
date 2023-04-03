f52e500eed29eb3f8fbae88a21139760a795b254
Jia <126303249+shangruJia@users.noreply.github.com>
2023-04-03 15:03:34 +0900

    Add notes and explanation.

--- Annotate query dataset/batch_effect_correction.r
+++ Annotate query dataset/batch_effect_correction.r
@@ -1,48 +1,40 @@
 library(Seurat)
 library(SeuratDisk)
 
-reference <- readRDS("reference.rds")
-Convert("./sample/sample.h5ad", dest = "h5seurat", overwrite = FALSE)
-query <- LoadH5Seurat("sample.h5seurat")
-gene_list <- unlist(read.csv("./pretrained/pretrained_genes.csv")["gene_ids"])
-# Select genes analyzed in the pretrained model in the reference dataset and query dataset
+# First set path, users can replace it with the folder where they store the reference and query datasets.
+path <- "/Usersdata/shangru/docker/pretrained_sample/"
+# This script requires three input file to generate the output file "merge_sct.h5ad", which is used in the annoation process.
+# When annotating customized dataset, users only have to change input 2 to the targeted dataset.
+# Load reference dataset, which can be downloaded directly from cellxgene website.
+# Input 1: Reference dataset.
+start <- proc.time()
+reference <- readRDS(file.path(path, "reference.rds"))
+# Then load query dataset. The sample query dataset is also provied in the Github repository.
+# Script of how the sample query dataset is sliced is available in the Additional folder.
+# Input 2: Query dataset.
+Convert(file.path(path, "sample.h5ad"), dest = "h5seurat", overwrite = FALSE)
+query <- LoadH5Seurat(file.path(path, "sample.h5seurat"))
+# Input 3: Genes analyzed in the pretrained model.
+gene_list <- unlist(read.csv(file.path(path, "pretrained_genes.csv"))["gene_ids"])
+# Slice the reference dataset and query dataset by genes analyzed in the pretrained model.
 reference <- subset(reference, features=gene_list)
 query <- subset(query, features=gene_list)
+# Add feature "orig" and set genes in the query dataset as variable genes.
 reference@meta.data["orig"] = "reference"
 query@meta.data["orig"] = "query"
 merge <- merge(reference, y=query)
 VariableFeatures(merge) <- query$RNA@data@Dimnames[[1]]
-#Perorm normalization
+# Perorm normalization.
 merge.list <- SplitObject(merge, split.by = "orig")
 merge.list <- lapply(X = merge.list, FUN = SCTransform, method = "glmGamPoi")
-#Perorm batch effect correction between the reference dataset and sample query dataset in the integration process
+# Perorm batch effect correction between the reference dataset and sample query dataset in the integration process.
 fq <- SelectIntegrationFeatures(object.list = merge.list, nfeatures = 3000)
 merge.list <- PrepSCTIntegration(object.list = merge.list, anchor.features = fq)
+# merge.list[1] is the reference dataset, use it as the reference set in the integration process. 
 merge.anchors <- FindIntegrationAnchors(object.list = merge.list, normalization.method = "SCT", anchor.features = fq, reference=1)
 sct <- IntegrateData(anchorset = merge.anchors, normalization.method = "SCT")
-SaveH5Seurat(sct, assay = "integrated", filename = "merge_sct.h5Seurat")
-Convert("merge_sct.h5Seurat", assay = "integrated", dest = "h5ad")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-sample = pd.read_csv("./sample/sample_query.csv", index_col=0)
-
-
+SaveH5Seurat(sct, assay = "integrated", filename = file.path(path,"merge_sct.h5Seurat"))
+# Output: "merge_sct.h5ad", which is used in the "test_on_query.ipynb" annoation notebook.
+Convert(file.path(path,"merge_sct.h5Seurat"), assay = "integrated", dest = "h5ad")
+# Calculate the whole time cost in the batch effect correction process.
+time_cost <- proc.time() - start
